26ee8776e8577934702a23e817c31f86db98d5bf
Meryx <anwar.haredy@gmail.com>
2023-11-25 20:52:28 +0300

    Initial Commit

---
+++ GymTrackerUITests/GymTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GymTrackerUITestsLaunchTests.swift
+//  GymTrackerUITests
+//
+//  Created by Anwar Haredy on 11/05/1445 AH.
+//
+
+import XCTest
+
+class GymTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
