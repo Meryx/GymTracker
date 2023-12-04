6cf77f2803cf88c13a061a654aa4e7b502ba0c65
Meryx <anwar.haredy@gmail.com>
2023-12-04 14:55:44 +0300

    Add exercise list

---
+++ GymTracker/ExerciseRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseRowView.swift
+//  GymTracker
+//
+//  Created by Anwar Haredy on 20/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ExerciseRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseRowView()
+}
